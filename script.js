3478351f4ab20f12dafcbd9903e08f1fd71ef337
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2024-03-30 23:45:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 // Complete the js code
 function Car(make, model) {
-	constructor(make, model) {
-		this.make = make;
-		this.model = model;
-	}
-
-	getMakeModel() {
-		return this.make + " " + this.model;
-	}
+	this.make = make;
+	this.model = model;
 }
+
+Car.prototype.getMakeMode = function() {
+	return `${this.make} ${this.model}`
+};
 
 function SportsCar(make, model, topSpeed) {
-	constructor(make, model, topSpeed) {
-		super(make, model);
-		this.topSpeed = topSpeed;
-	}
-
-	getTopSpeed() {
-		return this.topSpeed;
-	}
+	Car.call(this, make, model);
+	this.topSpeed = topSpeed;
 }
+
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function() {
+	return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
+
+
